cfe49fcaccc8fed8ee6fa2ab4458bc034ed2788b
Ethan Yan <eyyan@wisc.edu>
2024-06-18 15:44:19 -0400

    Initial web setup

---
+++ web/src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../components/Header";
+
+function Home() {
+  return (
+    <div>
+      <Header />
+      <p>Welcome to StrideSync!</p>
+    </div>
+  );
+}
+
+export default Home;
